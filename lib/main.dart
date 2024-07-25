0186d6df27f38d69af9e95552c5ea7d74416e1ca
Sachin <91337586+Sachinbgithub@users.noreply.github.com>
2024-07-26 01:01:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: 'AIzaSyDzO0YYrQuBT0eELo2vGe8d4vwaLA_TAKA',
-      appId: '1:981845607190:android:fe64029ade3c6b11240637',
+      apiKey: 'Add Yours',
+      appId: 'Add Yours',
       projectId: 'busdata-2024',
-      messagingSenderId: '981845607190',
+      messagingSenderId: 'Add Yours',
     ),
   );
 //   runApp( LoginPage());
